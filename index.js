b478cbdabb26a8239cf51a5bbc5b3f2818a99da1
priscilla <pdjarbeng@outlook.com>
2021-09-27 14:55:01 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(num1, num2){
+    console.log(num1/num2);
+} divide(2000,100);
+
+let square = (multiply) => console.log (multiply)
+ square(2*2)
+
+ let add = (sum) => console.log (sum)
+ add(5+2)
