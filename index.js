59cd1b95995ef20b4964eea2245dace18ede0cf0
priscilla <pdjarbeng@outlook.com>
2021-10-13 13:00:15 -0400

    commit tree 78fd1f1f2398fdefcd8c58acc1656322ae770bdf

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-function divide(num1, num2){
-    console.log(num1/num2);
-} divide(2000,100);
 
-let square = (multiply) => console.log (multiply)
- square(2*2)
 
- let add = (sum) => console.log (sum)
- add(5+2)
+const divide = (parameter1= 2000, parameter2= 100) => parameter1 / parameter2
+
+// const divide = (parameter1= 2000, parameter2= 100) => {
+//     console.log(`output ${parameter1}`)
+//     console.log(`output ${parameter2}`)
+//     return parameter1 / parameter2
+// }
+
+const square = (parameter1= 2, parameter2= 2) => parameter1 * parameter2
+
+const add = (parameter1= 2, parameter2= 2) => parameter1 + parameter2
+
